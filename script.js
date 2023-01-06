df3399738edf80394a58b164d93fe9d893b8fb39
Daniel <contact@danielurzola.com>
2023-01-06 02:14:04 -0500

    new commit

--- script.js
+++ script.js
@@ -3,12 +3,27 @@ const tabs = document.querySelectorAll('#Header .tabs span')
 
 textos.forEach(p => p.style.display = "none")
 textos[0].style.display = "block"
+tabs[0].classList.add('activo')
 
 for (let i = 0; i < 3; i++) {
     tabs[i].addEventListener('click',function() {
         textos.forEach(p => p.style.display = "none")
         textos[i].style.display = "block"
+        
+        tabs.forEach(tab => tab.classList.remove('activo'))
+        tabs[i].classList.add('activo')
+
     })
 }
 
+
+const typingEffect = new Typed('#textType',{
+    strings : ["my Space","my WebPage","my World","my Journey"],
+    loop : true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    startDelay: 500,
+    backDelay: 1500,
+    showCursor: true
+})
 
