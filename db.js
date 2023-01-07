5c308960c3da67687b4ad203ecd228ec59f2097f
Daniel <contact@danielurzola.com>
2023-01-07 12:31:58 -0500

    new commit

---
+++ db.js
@@ -0,0 +1,152 @@
+function bodyMessage(logo) {
+    if(logo.classList.contains("html")) {
+        return ( `
+                <p class="titlee">HTML</p>
+                <p class="prem">I have clear knowledge in these areas :</p>
+                <ul>
+                    <li>Creation of elements</li>
+                    <li>Async/Await</li>
+                    <li>Manipulation of Arrays</li>
+                    <li>Manipulation of Objects</li>
+                    <li>Functions</li>
+                    <li>Methods/Properties</li>
+                    <li>Events</li>
+                </ul>
+
+            `
+        )
+    }
+    if(logo.classList.contains("css")) {
+        return ( `
+                <p class="titlee">CSS</p>
+                <p class="prem">I have clear knowledge in these areas :</p>
+                <ul>
+                    <li>Promises</li>
+                    <li>Async/Await</li>
+                    <li>Manipulation of Arrays and Objects</li>
+                    <li>Functions</li>
+                    <li>Methods/Properties</li>
+                    <li>Events</li>
+                </ul>
+            `
+        )
+    }
+    if(logo.classList.contains("js")) {
+        return ( `
+                <p class="titlee">JavaScript</p>
+                <p class="prem">I have clear knowledge in these areas :</p>
+                <ul>
+                    <li>Promises</li>
+                    <li>Async/Await</li>
+                    <li>Manipulation of Arrays and Objects</li>
+                    <li>Functions</li>
+                    <li>Methods/Properties</li>
+                    <li>Events</li>
+                </ul>
+            `
+        )
+    }
+    if(logo.classList.contains("react")) {
+        return ( `
+                <p class="titlee">React JS</p>
+                <p class="prem">I have clear knowledge in these areas :</p>
+                <ul>
+                    <li>Promises</li>
+                    <li>Async/Await</li>
+                    <li>Manipulation of Arrays</li>
+                    <li>Manipulation of Objects</li>
+                    <li>Functions</li>
+                    <li>Methods/Properties</li>
+                    <li>Events</li>
+                </ul>
+            `
+        )
+    }
+    if(logo.classList.contains("sass")) {
+        return ( `
+                <p class="titlee">SASS</p>
+                <p class="prem">I have clear knowledge in these areas :</p>
+                <ul>
+                    <li>Promises</li>
+                    <li>Async/Await</li>
+                    <li>Manipulation of Arrays</li>
+                    <li>Manipulation of Objects</li>
+                    <li>Functions</li>
+                    <li>Methods/Properties</li>
+                    <li>Events</li>
+                </ul>
+            `
+        )
+    }
+    if(logo.classList.contains("git")) {
+        return ( `
+                <p class="titlee">GIT</p>
+                <p class="prem">I have clear knowledge in these areas :</p>
+                <ul>
+                    <li>Promises</li>
+                    <li>Async/Await</li>
+                    <li>Manipulation of Arrays</li>
+                    <li>Manipulation of Objects</li>
+                    <li>Functions</li>
+                    <li>Methods/Properties</li>
+                    <li>Events</li>
+                </ul>
+            `
+        )
+    }
+    if(logo.classList.contains("tailwind")) {
+        return ( `
+                <p class="titlee">Tailwind</p>
+                <p class="prem">I have clear knowledge in these areas :</p>
+                <ul>
+                    <li>Promises</li>
+                    <li>Async/Await</li>
+                    <li>Manipulation of Arrays</li>
+                    <li>Manipulation of Objects</li>
+                    <li>Functions</li>
+                    <li>Methods/Properties</li>
+                    <li>Events</li>
+                </ul>
+            `
+        )
+    }
+    if(logo.classList.contains("bootstrap")) {
+        return ( `
+                <p class="titlee">Bootstrap</p>
+                <p class="prem">I have clear knowledge in these areas :</p>
+                <ul>
+                    <li>Promises</li>
+                    <li>Async/Await</li>
+                    <li>Manipulation of Arrays</li>
+                    <li>Manipulation of Objects</li>
+                    <li>Functions</li>
+                    <li>Methods/Properties</li>
+                    <li>Events</li>
+                </ul>
+            `
+        )
+    }
+    if(logo.classList.contains("figma")) {
+        return ( `
+                <p class="titlee">Figma</p>
+                <p class="prem">I have clear knowledge in these areas :</p>
+                <ul>
+                    <li>Promises</li>
+                    <li>Async/Await</li>
+                    <li>Manipulation of Arrays</li>
+                    <li>Manipulation of Objects</li>
+                    <li>Functions</li>
+                    <li>Methods/Properties</li>
+                    <li>Events</li>
+                </ul>
+            `
+        )
+    }
+}
+
+
+export {
+    bodyMessage
+}
+
+
--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const typingEffect = new Typed('#textType',{
+    strings : ["my Space","my WebPage","my Portfolio","my Journey"],
+    loop : true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    startDelay: 500,
+    backDelay: 1500,
+    showCursor: true
+})
+
+
+
 const textos = document.querySelectorAll('#Header .text p')
 const tabs = document.querySelectorAll('#Header .tabs span')
 
@@ -13,17 +25,113 @@ for (let i = 0; i < 3; i++) {
         tabs.forEach(tab => tab.classList.remove('activo'))
         tabs[i].classList.add('activo')
 
-    })
+    })    
+}    
+
+
+
+
+
+const logos = document.querySelectorAll('#Header .techs .logo')
+import { bodyMessage } from "./db.js"
+
+let array = [];
+
+function hoverLogo(logo) {
+    if(array.length === 0) {
+        const element = document.createElement('div')
+        const imagen = logo.querySelector('img')
+
+        element.classList.add('hover-logo')
+        
+        element.innerHTML = `
+        <div class="img-div">
+            <img src="${imagen.src}"/>
+        </div>
+        <div class="body">
+            ${bodyMessage(logo)}
+        </div>
+        `
+
+        array.push(element)    
+        
+        let timeout = setTimeout(() => {
+            logo.appendChild(element)
+            setTimeout(() => {
+                element.style.opacity = "1"  
+                element.style.width = "400px"  
+                element.style.height = "500px"  
+            }, 500);
+        }, 500);
+        
+        logo.addEventListener('mouseleave',function(){
+            element.remove()
+            array.shift()
+            clearTimeout(timeout)
+        })
+    }
 }
 
 
-const typingEffect = new Typed('#textType',{
-    strings : ["my Space","my WebPage","my Portfolio","my Journey"],
-    loop : true,
-    typeSpeed: 100,
-    backSpeed: 80,
-    startDelay: 500,
-    backDelay: 1500,
-    showCursor: true
+
+logos.forEach(logo => {
+    logo.addEventListener('mouseenter',() => hoverLogo(logo))
 })
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const projects = document.querySelectorAll('#Projects .box')
+
+let arrayP = []
+
+function hoverProject(project) {
+    if(arrayP.length === 0) {
+        const element = document.createElement('div')
+        const title = project.querySelector('.title')
+
+        element.classList.add('hover-project')
+        
+        element.innerHTML = `
+        <div class="cabezera">
+            <p class="titleee">${title.textContent}</p>
+        </div>
+        <div class="tabss">
+            <span>Description</span>
+            <span>Instructions</span>
+        </div>
+        `
+
+        arrayP.push(element)    
+        
+        let timeout = setTimeout(() => {
+            project.appendChild(element)
+            setTimeout(() => {
+                element.style.opacity = "1"  
+                element.style.width = "400px"  
+                element.style.height = "550px"  
+            }, 500);
+        }, 500);
+        
+        project.addEventListener('mouseleave',function(){
+            element.remove()
+            arrayP.shift()
+            clearTimeout(timeout)
+        })
+    }
+}
+
+projects.forEach(project => {
+    project.addEventListener('mouseenter',() => hoverProject(project))
+})
